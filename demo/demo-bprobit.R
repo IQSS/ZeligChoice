778cabea4580f7a9fc5fb141df8ecfc12c5ca46f
cchoirat <cchoirat@gmail.com>
2014-11-16 17:34:24 -0500

    Basic support of #1, #2, #5

--- demo/demo-bprobit.R
+++ demo/demo-bprobit.R
@@ -17,9 +17,10 @@ data(sanction)
 z5 <- zbprobit$new()
 z5$zelig(cbind(import, export) ~ coop + cost + target, data = sanction)
 z5
-z5$setx(cost=1)
+z5$setx(cost = 1)
 set.seed(42)
-z5$sim(num=1000)
+z5$sim(num = 1000)
 z5$summarize()
 z5$cite()
 
+# z5$zelig(list(import ~ coop + cost + target, export ~ coop + cost + target), data = sanction)
