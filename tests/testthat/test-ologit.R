93334862fa7fc6d17dc340fd437568230b8b0cfb
christophergandrud <christopher.gandrud@gmail.com>
2017-02-09 07:55:03 +0100

    version bump
    readme badges update

--- tests/testthat/test-ologit.R
+++ tests/testthat/test-ologit.R
@@ -1,3 +1,8 @@
-z <- zologit$new()
-test <- z$mcunit(minx=0, maxx=2, plot=FALSE)
-expect_true(test)
+#### Ordered Logistic Regression Tests ####
+
+# REQUIRE TEST Monte Carlo ologit ----------------------------------------------
+test_that('REQUIRE TEST Monte Carlo ologit', {
+    z <- zologit$new()
+    test <- z$mcunit(minx = 0, maxx = 2, plot = FALSE)
+    expect_true(test)
+})
