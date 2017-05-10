f5df571da905cc5d07668fc9a38e1e4201d1bd1d
christophergandrud <christopher.gandrud@gmail.com>
2017-05-10 10:58:16 -0400

    vignette URL update

--- R/model-ologit.R
+++ R/model-ologit.R
@@ -1,6 +1,6 @@
 #' Ordinal Logistic Regression for Ordered Categorical Dependent Variables
 #'
-#' Vignette: \url{http://docs.zeligproject.org/article/zeligchoice-ologit.html}
+#' Vignette: \url{http://docs.zeligproject.org/articles/zeligchoice_ologit.html}
 #' @import methods
 #' @export Zelig-ologit
 #' @exportClass Zelig-ologit
@@ -23,7 +23,7 @@ zologit$methods(
       return(tmp1)
     }
     .self$wrapper <- "ologit"
-    .self$vignette.url <- "http://docs.zeligproject.org/article/zeligchoice-ologit.html"
+    .self$vignette.url <- "http://docs.zeligproject.org/articles/zeligchoice_ologit.html"
   }
 )
 
